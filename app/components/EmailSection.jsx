e2f9170263e4d336fc88280c65a05c25d277784f
Abdulbaqi <abdulbaqhussain2018@gmail.com>
2024-12-25 02:47:42 -0800

    string modified

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,9 +1,9 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import GithubIcon from "../../public/github-icon.svg";
 import LinkedinIcon from "../../public/linkedin-icon.svg";
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link"
+import Image from "next/image"
 const EmailSection = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
@@ -21,7 +21,7 @@ const EmailSection = () => {
       const options = {
         // The method is POST because we are sending data.
         method: "POST",
-        // Tell the server we're sending JSON.
+        // Tell the server we"re sending JSON.
         headers: {
           "Content-Type": "application/json",
         },
@@ -36,48 +36,48 @@ const EmailSection = () => {
        }
     }
 
-  return <section  className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 realtive'>
+  return <section  className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 realtive">
      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent
       rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-    <div className='z-10'>
-        <h5 className='text-xl font-bold text-white my-2'>Let's connect</h5>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+    <div className="z-10">
+        <h5 className="text-xl font-bold text-white my-2">Let"s connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
         {" "}
           I am currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
-         <div className='socials flex flex-row gap-2'>
-            <Link href='/'>
-               <Image src={GithubIcon} alt='git-image'/>
+         <div className="socials flex flex-row gap-2">
+            <Link href="/">
+               <Image src={GithubIcon} alt="git-image"/>
             </Link>
-            <Link href='/'>
-               <Image src={LinkedinIcon} alt='linkedln-image'/>
+            <Link href="/">
+               <Image src={LinkedinIcon} alt="linkedln-image"/>
             </Link>
          </div>
     </div>
     <div>
-     <form action="" className='flex flex-col' onSubmit={handleSubmit}>
-    <div className='mb-6'>
-    <label htmlFor="email" type="email" className='text-white block text-sm mb-2 font-medium'>Your Email</label>
+     <form action="" className="flex flex-col" onSubmit={handleSubmit}>
+    <div className="mb-6">
+    <label htmlFor="email" type="email" className="text-white block text-sm mb-2 font-medium">Your Email</label>
      <input
-     name='email'
-      type='email'
-      id='email'
-       placeholder='youemail.com'
+     name="email"
+      type="email"
+      id="email"
+       placeholder="youemail.com"
         required
-        className='bg-[#18191E] border border-[33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded p-2.5 block w-full '
+        className="bg-[#18191E] border border-[33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded p-2.5 block w-full "
         />
         </div>
-           <div className='mb-6'>
-       <label htmlFor="subject" type="text" className='text-white block mb-2 text-sm font-medium'>Subject</label>
+           <div className="mb-6">
+       <label htmlFor="subject" type="text" className="text-white block mb-2 text-sm font-medium">Subject</label>
      <input
-     name='subject'
-      type='text'
-      id='subject'
-       placeholder='Just saying hi'
+     name="subject"
+      type="text"
+      id="subject"
+       placeholder="Just saying hi"
         required
-        className='bg-[#18191E] border border-[33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded p-2.5 block w-full '
+        className="bg-[#18191E] border border-[33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded p-2.5 block w-full "
         />
         </div>
         <div className="mb-6">
